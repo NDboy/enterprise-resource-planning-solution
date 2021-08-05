96ceeb303bd4477de1c0c26ed411961e4083d5bd
NDboy <endre.karsai@gmail.com>
2021-08-05 18:29:51 +0200

    Initial commit

---
+++ src/main/java/erp/EnterpriseResourcePlanningSolutionApplication.java
@@ -0,0 +1,13 @@
+package erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseResourcePlanningSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseResourcePlanningSolutionApplication.class, args);
+    }
+
+}
